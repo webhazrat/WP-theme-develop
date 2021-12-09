9999cccf586128c20fc3293448dc965ccdf9707a
Md. Hazrat Ali <mdhazrat445891@gmail.com>
2021-12-09 19:15:02 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
-<!-- for get header template -->
+<?php
+//for get header template
 get_header(string name, array $args = array());
 //echo $args['key'];
 
-<!-- for get footer template -->
+//for get footer template
 get_footer(string name, array $args = array());
