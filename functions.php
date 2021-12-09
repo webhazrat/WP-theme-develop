f17652e59390e20f8b42aadb1ab748358acdc3d2
Md. Hazrat Ali <mdhazrat445891@gmail.com>
2021-12-09 19:16:30 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 //for get header template
 get_header(string name, array $args = array());
+// name parameter user for load specific header template.
 //echo $args['key'];
 
 //for get footer template
