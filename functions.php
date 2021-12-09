4175b316feb25133e4429f9c1a045641500f416c
Md. Hazrat Ali <mdhazrat445891@gmail.com>
2021-12-09 19:13:51 +0600

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,6 @@
+<!-- for get header template -->
+get_header(string name, array $args = array());
+//echo $args['key'];
+
+<!-- for get footer template -->
+get_footer(string name, array $args = array());
